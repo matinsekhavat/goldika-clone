9da5521784451bb00f428c95b5149bfb79ea805d
matinsekhavat <matinhyphen@gmail.com>
2024-08-06 02:01:58 +0330

    finished version 1.0 Header.tsx file

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,12 @@
 import Link from "next/link";
 import Logo from "./Logo";
-import { AlignJustify } from "lucide-react";
+import NavBar from "./NavBar";
 
 function Header() {
   return (
-    <header className="w-full fixed top-0 left-0 right-0 ">
-      <div className="container-fluid flex items-center justify-between py-3  bg-slate-950">
-        {/* Mobile mode */}
-        <nav className="flex items-center gap-4">
-          <button>
-            <AlignJustify />
-          </button>
-          <Logo />
-        </nav>
+    <header className="w-full fixed top-0 left-0 right-0 bg-slate-950 ">
+      <div className="container-fluid flex items-center justify-between py-3  ">
+        <NavBar />
         {/* auth Badge */}
         <Link href="/auth" className="">
           <span className="text-[14px] border text-yellow-500 border-yellow-700 p-1 rounded-lg hover:bg-yellow-600/10  transition-all font-semibold">
