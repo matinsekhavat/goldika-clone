d5df23fffda888b85360935f60fe2016665ff5d8
matinsekhavat <matinhyphen@gmail.com>
2024-08-18 17:24:45 +0330

    implement helper class for modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -7,14 +7,13 @@ type ModalProps = {
 
 const Modal = forwardRef<HTMLDivElement, ModalProps>(
   ({ children, isOpen }: ModalProps, ref) => {
-    if (!isOpen) return null;
     return (
       <div
         ref={ref}
-        className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 min-h-96 max-h-[500px] overflow-auto   max-w-xl w-[100%] transition-all duration-300 rounded-md p-2"
+        className={`fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10 min-h-96 max-h-[500px] overflow-auto h-[-webkit-fill-available] max-w-xl w-[100%] rounded-md p-2 modal ${
+          isOpen ? "isOpen" : ""
+        }`}
         style={{
-          opacity: isOpen ? 1 : 0,
-          visibility: isOpen ? "visible" : "hidden",
           transition: "opacity 300ms, visibility 300ms",
         }}
       >
